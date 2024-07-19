836b87571600e95445156b77b5d0dcf6fa2b9edc
WhitePaper <55529807+ijb0708@users.noreply.github.com>
2024-07-19 10:59:41 +0900

    Time: 22 ms (7.85%) | Memory: 14.4 MB (16.09%) - LeetSync

---
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+        
+        vector<int> answer;
+        int col[51] = {0,};
+
+        for(int i=0; i<matrix[0].size(); i++) {
+            int maxIdx = 0;
+            for(int j=0; j<matrix.size(); j++) {
+                if(matrix[j][i] > matrix[maxIdx][i]) {
+                    maxIdx = j;
+                }
+            }
+            col[i] = maxIdx;
+        }
+
+        for(int i=0; i<3; i++) cout<<col[i]<<" ";
+
+        for(int i=0; i<matrix.size(); i++) {
+            int minIdx = 0;
+            for(int j=0; j<matrix[0].size(); j++) {
+                if(matrix[i][j] < matrix[i][minIdx]) {
+                    minIdx = j;
+                }
+            }
+            if(col[minIdx] == i) {
+                answer.push_back(matrix[i][minIdx]);
+            }
+        }
+        return answer;
+    }
+};
