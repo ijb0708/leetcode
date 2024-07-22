06c4703882c75e1082352e8e12952209a0620c0c
WhitePaper <55529807+ijb0708@users.noreply.github.com>
2024-07-22 17:51:59 +0900

    Time: 55 ms (51.28%) | Memory: 48.9 MB (77.82%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    
+    if(str1 + str2 != str2 + str1)
+        return "";
+    
+    function gcd(a, b) {
+        if(b==0) return a
+        return gcd(b, parseInt(a%b));
+    }
+    // console.log(gcd(10, 5))
+    const gcdLen = gcd(str1.length, str2.length)
+    return str1.substr(0, gcdLen);
+};
