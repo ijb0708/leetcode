fa61b2ca1abe7e42bbb79f3c336f9f884550da28
WhitePaper <55529807+ijb0708@users.noreply.github.com>
2024-07-24 17:05:09 +0900

    Time: 82 ms (34.79%) | Memory: 54.3 MB (47.09%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let p1 = 0;
+    let p2 = 1;
+    while(p2 < nums.length) {
+        if(nums[p1] == 0 && nums[p2] != 0) {
+            const temp = nums[p1];
+            nums[p1] = nums[p2];
+            nums[p2] = temp;
+            p1++;
+        }
+        if(nums[p1] != 0 && nums[p2] == 0) {
+            p1++;
+        }
+        if(nums[p1] != 0 && nums[p2] != 0) {
+            p1++;
+        }
+        p2++;
+    }
+    return nums
+};
