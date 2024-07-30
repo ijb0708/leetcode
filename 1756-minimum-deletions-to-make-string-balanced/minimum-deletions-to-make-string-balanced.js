e132da5f4dea2e0338ca5b4eb5b812e6c67bc909
WhitePaper <55529807+ijb0708@users.noreply.github.com>
2024-07-30 22:09:10 +0900

    Time: 187 ms (9.09%) | Memory: 69.2 MB (9.09%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minimumDeletions = function(s) {
+    
+    const cacheA = Array(s.length).fill(0);
+    const cacheB = Array(s.length).fill(0);
+
+    cacheA[0] = s[0] == 'a' ? 1 : 0;
+    cacheB[0] = s[0] == 'b' ? 1 : 0;
+    for(let i=1; i<s.length; i++) {
+        if(s[i] == 'a') cacheA[i] = cacheA[i-1] + 1;
+        else cacheA[i] = cacheA[i-1];
+
+        if(s[i] == 'b') cacheB[i] = cacheB[i-1] + 1;
+        else cacheB[i] = cacheB[i-1];
+    }
+    console.log(cacheA)
+    console.log(cacheB)
+
+    // const balancedBars = [0];
+
+    let answer = 100001;
+    
+    answer = Math.min(answer, cacheA[s.length-1])
+    for(let i=1; i<s.length; i++) {
+        if(s[i-1] != s[i]) {
+            // balancedBars.push(i);
+            answer = Math.min(answer, cacheB[i-1] + cacheA[s.length-1] - cacheA[i])
+            // answer = Math.min(answer, (i - cacheA[i]) + (s.length - i - cacheB[s.length] - cacheB[i]));
+        }
+    }
+    answer = Math.min(answer, cacheB[s.length-1])
+
+    return answer >= 100001 ? 0 : answer;
+};
