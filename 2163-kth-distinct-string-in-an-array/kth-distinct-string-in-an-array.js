8012646549cef8927aaae1464f5e5d0ec9cb02a4
WhitePaper <55529807+ijb0708@users.noreply.github.com>
2024-08-06 02:40:55 +0900

    Time: 75 ms (37.63%) | Memory: 57.1 MB (6.19%) - LeetSync

---
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} arr
+ * @param {number} k
+ * @return {string}
+ */
+var kthDistinct = function(arr, k) {
+    
+    const countMap = {};
+    for(const item of arr) {
+        if(countMap[item] == null) 
+            countMap[item] = 0;
+        countMap[item]++;
+    }
+
+    console.log(countMap);
+
+    const arrFilter = arr.filter(function(item) {
+        if(countMap[item] == 1)
+            return true;
+        return false;
+    });
+    
+    return arrFilter.length > k-1 ? arrFilter[k-1] : "";
+};
