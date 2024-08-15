b9591650b1c7163aef44d469261d0c7ae72817f5
WhitePaper <55529807+ijb0708@users.noreply.github.com>
2024-08-15 15:23:53 +0900

    Time: 162 ms (5.13%) | Memory: 58.3 MB (5.31%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function(candidates, target) {
+    const answer = [];
+    candidates.sort(function(a, b) {
+        return a - b;
+    });
+
+    function dfs(idx, arr, localTarget) {
+        console.log(arr, localTarget);
+        if(localTarget == 0) {
+            answer.push([...arr])
+        }
+        for(let i=idx; i<candidates.length; i++) {
+            if(i > idx && candidates[i] == candidates[i-1]) continue;
+            if(candidates[i] > localTarget) break;
+
+            arr.push(candidates[i]);
+            dfs(i+1, arr, localTarget - candidates[i]);
+            arr.pop();
+        }
+
+    }
+    dfs(0, [], target);
+    console.log(answer);
+    return answer;
+};
