57b0cf21223e601b95ea825614052997dc108a77
WhitePaper <55529807+ijb0708@users.noreply.github.com>
2025-04-07 15:18:22 +1200

    Time: 231 ms (31.99%) | Memory: 65.7 MB (50.29%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canPartition = function(nums) {
+    
+    let total = 0;
+    for(const num of nums) {
+        total += num;
+    }
+    if(total % 2 != 0)
+        return false;
+    
+    const cache = Array.from({length:total / 2 + 1}, ()=>false);
+
+    console.log(cache)
+
+    cache[0] = true;
+    for(const num of nums) {
+        for(let i = total / 2; i >= num; i--) {
+            cache[i] = cache[i] || cache[i - num];
+        }
+        console.log(cache);
+    }
+    console.log(cache);
+    return cache[total / 2];
+};
